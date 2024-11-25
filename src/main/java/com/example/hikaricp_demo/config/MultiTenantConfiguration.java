4132b470f351d4c9574addcdf92d7c0c654586e1
Thanh Dat <dattt2@unitech.vn>
2024-11-25 15:45:42 +0700

    change file .properties to .yml and aply HikariCP

--- src/main/java/com/example/hikaricp_demo/config/MultiTenantConfiguration.java
+++ src/main/java/com/example/hikaricp_demo/config/MultiTenantConfiguration.java
@@ -1,16 +1,20 @@
 package com.example.hikaricp_demo.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.yaml.snakeyaml.Yaml;
 
 import javax.sql.DataSource;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,26 +26,34 @@ public class MultiTenantConfiguration {
     @Value("${defaultTenant}")
     private String defaultTenant;
 
+    @SuppressWarnings("unchecked")
     @Bean
-    @ConfigurationProperties(prefix = "tenants")
     public DataSource dataSource() {
         File[] files = Paths.get("allTenants").toFile().listFiles();
         Map<Object, Object> resolvedDataSources = new HashMap<>();
 
         assert files != null;
         for (File propertyFile : files) {
-            Properties tenantProperties = new Properties();
-            DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+            Yaml yaml = new Yaml();
+            Map<String, Object> tenantProperties;
 
-            try {
-                tenantProperties.load(new FileInputStream(propertyFile));
-                String tenantId = tenantProperties.getProperty("name");
+            try (InputStream inputStream = new FileInputStream(propertyFile)) {
+                tenantProperties = yaml.load(inputStream);
 
-                dataSourceBuilder.driverClassName(tenantProperties.getProperty("datasource.driver-class-name"));
-                dataSourceBuilder.username(tenantProperties.getProperty("datasource.username"));
-                dataSourceBuilder.password(tenantProperties.getProperty("datasource.password"));
-                dataSourceBuilder.url(tenantProperties.getProperty("datasource.url"));
-                resolvedDataSources.put(tenantId, dataSourceBuilder.build());
+                String tenantId = (String) tenantProperties.get("name");
+                Map<String, Object> datasourceProperties = (Map<String, Object>) tenantProperties.get("datasource");
+
+                HikariConfig hikariConfig = new HikariConfig();
+                hikariConfig.setDriverClassName((String) datasourceProperties.get("driver-class-name"));
+                hikariConfig.setJdbcUrl((String) datasourceProperties.get("url"));
+                hikariConfig.setUsername((String) datasourceProperties.get("username"));
+                hikariConfig.setPassword((String) datasourceProperties.get("password"));
+//                hikariConfig.setSchema((String) datasourceProperties.get("schema"));
+//                hikariConfig.setMaximumPoolSize(10);
+//                hikariConfig.setConnectionTimeout(30000);
+
+                HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+                resolvedDataSources.put(tenantId, dataSource);
             } catch (IOException exp) {
                 throw new RuntimeException("Problem in tenant datasource:" + exp);
             }
@@ -50,9 +62,7 @@ public class MultiTenantConfiguration {
         AbstractRoutingDataSource dataSource = new MultiTenantDataSource();
         dataSource.setDefaultTargetDataSource(resolvedDataSources.get(defaultTenant));
         dataSource.setTargetDataSources(resolvedDataSources);
-
         dataSource.afterPropertiesSet();
         return dataSource;
     }
-
 }
